f10d06f9c9e023972709938408088d80d6d38f45
Davi Moreira <davi.moreira@vismaconsulting.fi>
2018-11-15 14:23:13 +0200

    updated version

--- main.bundle.js
+++ main.bundle.js
@@ -20,14 +20,14 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /***/ "./src/app/app.component.css":
 /***/ (function(module, exports) {
 
-module.exports = ".jumbotron {\n  padding: 2rem 1rem;\n  margin-bottom: 2rem;\n  background-color: #e9ecef;\n  border-radius: 0 !important;\n  height: 100vh;\n}\n\n.app-header {\n  background-color: #aeaeae;\n}\n\n.events-row {\n  height: 120px;\n  position: fixed;\n  right: 30px;\n  bottom: 100px;\n  left: 20px;\n}\n\n.event-0 {\n  background-color: #f5f5f5;\n}\n\n.event-1 {\n  background-color: #eeeeee;\n}\n\n.event-2 {\n  background-color: #e0e0e0;\n}\n\n.event-3 {\n  background-color: #bdbdbd;\n}\n\n.event-4 {\n  background-color: #9e9e9e;\n}\n"
+module.exports = "#toggle {\n  width: 350px;\n  background: #ccc;\n  display: none;\n  z-index: 100;\n  float: left;\n  position: fixed;\n}\n\n.jumbotron {\n  padding: 2rem 1rem;\n  margin-bottom: 2rem;\n  background-color: #e9ecef;\n  border-radius: 0 !important;\n  height: 100vh;\n}\n\n.events-row {\n  position: fixed;\n  right: 30px;\n  bottom: 50px;\n  left: 20px;\n}\n\n.app-header {\n  width: 100%;\n  background-color: #555555;\n  color: #fafafa;\n  cursor: pointer;\n}\n\n.app-header-BLUES {\n  background-color: #00897b;\n}\n\n.event-0-BLUES {\n  background-color: #e0f2f1;\n}\n\n.event-1-BLUES {\n  background-color: #b2dfdb;\n}\n\n.event-2-BLUES {\n  background-color: #80cbc4;\n}\n\n.event-3-BLUES {\n  background-color: #4db6ac;\n}\n\n.event-4-BLUES {\n  background-color: #26a69a;\n}\n\n.app-header-SOUL {\n  background-color: #00acc1;\n}\n\n.event-0-SOUL {\n  background-color: #e0f7fa;\n}\n\n.event-1-SOUL {\n  background-color: #b2ebf2;\n}\n\n.event-2-SOUL {\n  background-color: #80deea;\n}\n\n.event-3-SOUL {\n  background-color: #4dd0e1;\n}\n\n.event-4-SOUL {\n  background-color: #26c6da;\n}\n\n.app-header-JAZZ {\n  background-color: #8e24aa;\n}\n\n.event-0-JAZZ {\n  background-color: #f3e5f5;\n}\n\n.event-1-JAZZ {\n  background-color: #e1bee7;\n}\n\n.event-2-JAZZ {\n  background-color: #ce93d8;\n}\n\n.event-3-JAZZ {\n  background-color: #ba68c8;\n}\n\n.event-4-JAZZ {\n  background-color: #ab47bc;\n}\n\n.app-header-ROCK {\n  background-color: #494949;\n}\n\n.event-0-ROCK {\n  background-color: #fafafa;\n}\n\n.event-1-ROCK {\n  background-color: #f5f5f5;\n}\n\n.event-2-ROCK {\n  background-color: #eeeeee;\n}\n\n.event-3-ROCK {\n  background-color: #e0e0e0;\n}\n\n.event-4-ROCK {\n  background-color: #bdbdbd;\n}\n\n.app-header-JOIKU {\n  background-color: #039be5;\n}\n\n.event-0-JOIKU {\n  background-color: #e1f5fe;\n}\n\n.event-1-JOIKU {\n  background-color: #b3e5fc;\n}\n\n.event-2-JOIKU {\n  background-color: #81d4fa;\n}\n\n.event-3-JOIKU {\n  background-color: #4fc3f7;\n}\n\n.event-4-JOIKU {\n  background-color: #29b6f6;\n}\n"
 
 /***/ }),
 
 /***/ "./src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"row m-0 app-header\" (click)=\"showMenu()\">\n  <div class=\"col mb-5\">\n  </div>\n</div>\n\n<div id=\"toggle\">\n  <span class=\"dropdown-item-text\">\n    <i class=\"fas fa-cog\"></i>\n    Settings\n  </span>\n\n  <div class=\"dropdown-divider\"></div>\n\n  <a class=\"dropdown-item\" href=\"#\" *ngIf=\"!isSignedIn\" (click)=\"handleAuthClick()\">\n    <i class=\"fab fa-google\"></i>\n    Authorize\n  </a>\n  <a class=\"dropdown-item\" href=\"#\" *ngIf=\"isSignedIn\" (click)=\"handleSignoutClick()\">\n    <i class=\"fas fa-sign-out-alt\"></i>\n    Sign Out\n  </a>\n  <a class=\"dropdown-item\" href=\"#\" *ngIf=\"isSignedIn\" (click)=\"fetchEvents()\">\n    <i class=\"fas fa-cloud-download-alt\"></i>\n    Fetch events\n  </a>\n\n  <div class=\"dropdown-divider\"></div>\n\n  <span class=\"dropdown-item-text\">\n    <i class=\"fas fa-th-large\"></i>\n    Rooms\n  </span>\n\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(JAZZ)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\" checked>\n      <label class=\"form-check-label\">\n        Jazz\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(SOUL)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Soul\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(ROCK)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Rock\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(BLUES)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Blues\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(JOIKU)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Joiku\n      </label>\n    </div>\n  </a>\n\n  <div class=\"dropdown-divider\"></div>\n\n  <a class=\"dropdown-item\" href=\"#\" (click)=\"showMenu()\">\n    Close\n  </a>\n\n</div>\n\n<div class=\"row\" *ngIf=\"events.length > 0\">\n  <div class=\"col\">\n\n    <ng-container *ngIf=\"isFree\">\n      <div class=\"jumbotron\">\n        <h1 class=\"display-1 mt-5\">{{roomName}}</h1>\n        <h1 class=\"display-4\">Free</h1>\n        <div class=\"row events-row\">\n          <ng-container *ngFor=\"let event of events; let i = index\">\n            <div class=\"col p-2 mr-1 event-{{i}}\">\n              <div>{{event.summary}}</div>\n              <div>{{event.start | date : 'dd.MM.yyyy HH:mm'}}</div>\n              <div>{{event.end | date : 'dd.MM.yyyy HH:mm'}}</div>\n            </div>\n          </ng-container>\n        </div>\n      </div>\n    </ng-container>\n\n    <ng-container *ngIf=\"!isFree && nowEvent\">\n      <div class=\"jumbotron\">\n        <h1 class=\"display-1 mt-5\">{{roomName}}</h1>\n        <h1 class=\"display-4\">{{nowEvent.summary}}</h1>\n        <p class=\"lead\">\n          {{nowEvent.start | date : 'HH:mm'}} - {{nowEvent.end | date : 'HH:mm'}}\n          <br />\n          {{nowEvent.creator.displayName}}\n        </p>\n        <div class=\"row events-row\">\n          <ng-container *ngFor=\"let event of events; let i = index\">\n            <div class=\"col p-2 mr-1 event-{{i}}\">\n              <div>{{event.summary}}</div>\n              <div>{{event.start | date : 'dd.MM.yyyy HH:mm'}}</div>\n              <div>{{event.end | date : 'dd.MM.yyyy HH:mm'}}</div>\n            </div>\n          </ng-container>\n        </div>\n      </div>\n    </ng-container>\n\n  </div>\n</div>\n"
+module.exports = "<div class=\"app-header app-header-{{roomName}} p-2\" (click)=\"showMenu()\">\n  <i class=\"fas fa-bars fa-3x\"></i>\n</div>\n\n<div id=\"toggle\">\n  <span class=\"dropdown-item-text\">\n    <i class=\"fas fa-cog\"></i>\n    Settings\n  </span>\n\n  <div class=\"dropdown-divider\"></div>\n\n  <a class=\"dropdown-item\" href=\"#\" *ngIf=\"!isSignedIn\" (click)=\"handleAuthClick()\">\n    <i class=\"fab fa-google\"></i>\n    Authorize\n  </a>\n  <a class=\"dropdown-item\" href=\"#\" *ngIf=\"isSignedIn\" (click)=\"handleSignoutClick()\">\n    <i class=\"fas fa-sign-out-alt\"></i>\n    Sign Out\n  </a>\n  <a class=\"dropdown-item\" href=\"#\" *ngIf=\"isSignedIn\" (click)=\"fetchEvents()\">\n    <i class=\"fas fa-cloud-download-alt\"></i>\n    Fetch events\n  </a>\n\n  <div class=\"dropdown-divider\"></div>\n\n  <span class=\"dropdown-item-text\">\n    <i class=\"fas fa-th-large\"></i>\n    Rooms\n  </span>\n\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(JAZZ)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\" checked>\n      <label class=\"form-check-label\">\n        Jazz\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(SOUL)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Soul\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(ROCK)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Rock\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(BLUES)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Blues\n      </label>\n    </div>\n  </a>\n  <a class=\"dropdown-item\" (click)=\"chooseRoom(JOIKU)\">\n    <div class=\"form-check\">\n      <input class=\"form-check-input\" type=\"radio\" name=\"exampleRadios\" value=\"option1\">\n      <label class=\"form-check-label\">\n        Joiku\n      </label>\n    </div>\n  </a>\n\n  <div class=\"dropdown-divider\"></div>\n\n  <a class=\"dropdown-item\" href=\"#\" (click)=\"showMenu()\">\n    Close\n  </a>\n\n</div>\n\n<ng-container *ngIf=\"events.length > 0\">\n    <ng-container *ngIf=\"isFree\">\n      <div class=\"jumbotron\">\n        <h1 class=\"display-1 mt-2\">{{roomName}}</h1>\n        <h1 class=\"display-3\">Vapaa</h1>\n        <p class=\"lead\">Seuraava: {{events[0].summary}}, {{resolveNextEvent()}} klo {{events[0].start | date : 'HH:mm'}}</p>\n        <div class=\"row events-row\">\n          <ng-container *ngFor=\"let event of events; let i = index\">\n            <div class=\"col p-2 mr-1 event-{{i}}-{{roomName}}\">\n              <div>{{event.summary}}</div>\n              <div class=\"mt-4\">\n                <span class=\"text-capitalize\">{{resolveEvent(event)}}</span>\n                klo {{event.start | date : 'HH:mm'}} - {{event.end | date : 'HH:mm'}}\n              </div>\n            </div>\n          </ng-container>\n        </div>\n      </div>\n    </ng-container>\n\n    <ng-container *ngIf=\"!isFree && nowEvent\">\n      <div class=\"jumbotron\">\n        <h1 class=\"display-1 mt-2\">{{roomName}}</h1>\n        <h1 class=\"display-4\">{{nowEvent.summary}}</h1>\n        <p class=\"lead\">\n          {{nowEvent.start | date : 'HH:mm'}} - {{nowEvent.end | date : 'HH:mm'}}\n          <br />\n          {{nowEvent.creator.displayName}}\n        </p>\n        <p class=\"lead\">Seuraava: {{events[0].summary}}, {{resolveNextEvent()}} klo {{events[0].start | date : 'HH:mm'}}</p>\n        <div class=\"row events-row\">\n          <ng-container *ngFor=\"let event of events; let i = index\">\n            <div class=\"col p-2 mr-1 event-{{i}}-{{roomName}}\">\n              <div>{{event.summary}}</div>\n              <div class=\"mt-4\">\n                <span class=\"text-capitalize\">{{resolveEvent(event)}}</span>\n                klo {{event.start | date : 'HH:mm'}} - {{event.end | date : 'HH:mm'}}\n              </div>\n            </div>\n          </ng-container>\n        </div>\n      </div>\n    </ng-container>\n</ng-container>\n"
 
 /***/ }),
 
@@ -103,13 +103,10 @@ var AppComponent = /** @class */ (function () {
                     self.events[i].end = end;
                 }
             }
-            console.log('fetching events end');
             self.checkIsFree(response.result.items);
         });
     };
     AppComponent.prototype.fetchEvents = function () {
-        console.log('is signed ' + this.isSignedIn);
-        console.log('fetching events');
         if (this.isSignedIn) {
             this.listUpcomingEvents();
         }
@@ -161,6 +158,25 @@ var AppComponent = /** @class */ (function () {
             }
         }
     };
+    AppComponent.prototype.resolveNextEvent = function () {
+        var event = this.events[0];
+        return this.resolveEvent(event);
+    };
+    AppComponent.prototype.resolveEvent = function (event) {
+        var now = new Date();
+        var start = new Date(event.start);
+        var day = '';
+        if (start.getDay() === now.getDay()) {
+            day = 'tänään';
+        }
+        else if (start.getDay() === now.getDay() + 1) {
+            day = 'huomenna';
+        }
+        else {
+            day = start.getDate() + '.' + (start.getMonth() + 1);
+        }
+        return day;
+    };
     AppComponent.prototype.showMenu = function () {
         $('#toggle').toggle('slide');
     };
@@ -173,11 +189,10 @@ var AppComponent = /** @class */ (function () {
         var self = this;
         setInterval(function () {
             var date = new Date();
-            console.log(date.getMinutes());
-            if (date.getMinutes() % 2 === 0) {
+            if (date.getMinutes() % 5 === 0) {
                 self.fetchEvents();
             }
-        }, 60000);
+        }, 10000);
     };
     AppComponent = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
