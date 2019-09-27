385832619fc9daaf185399e68363f3afdd72d4d0
Davi Moreira <davi.moreira@vismaconsulting.fi>
2019-09-27 11:27:07 +0300

    adding fucntion

--- my-helsinki/scripts.js
+++ my-helsinki/scripts.js
@@ -6,3 +6,16 @@ let filter = (tag) => {
     scrollTop: $('.slider-element').offset().top
   }, 500);
 };
+
+let fetchNearMe = () => {
+  let coords;
+  let listElement = document.querySelector('list-element');
+  listElement.shadowRoot.querySelector('.fa-spin').style.visibility = 'visible';
+  navigator.geolocation.getCurrentPosition(position => {
+    coords = position;
+    listElement.fetchNearMe(coords);
+  });
+  $('html, body').animate({
+    scrollTop: $('.slider-element').offset().top
+  }, 500);
+}
