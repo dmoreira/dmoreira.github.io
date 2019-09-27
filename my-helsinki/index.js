6181a5a52aada08b08b5375f024ea1fa7c9ef819
Davi Moreira <davi.moreira@vismaconsulting.fi>
2019-09-27 11:11:30 +0300

    added function

--- my-helsinki/index.js
+++ my-helsinki/index.js
@@ -1,11 +1,21 @@
-import "./src/web-components/slider-element.js";
-import "./src/web-components/list-element.js";
-import "./src/web-components/item-element.js";
-$(document).ready(function () {
-  $('.scroll-main-slider').click(function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-      scrollTop: $('.main-slider').offset().top
-    }, 500);
+let filter = (tag) => {
+  let listElement = document.querySelector('list-element');
+  listElement.setAttribute("tag", tag);
+  listElement.filter();
+  $('html, body').animate({
+    scrollTop: $('.slider-element').offset().top
+  }, 500);
+};
+
+let fetchNearMe = () => {
+  let coords;
+  let listElement = document.querySelector('list-element');
+  listElement.shadowRoot.querySelector('.fa-spin').style.visibility = 'visible';
+  navigator.geolocation.getCurrentPosition(position => {
+    coords = position;
+    listElement.fetchNearMe(coords);
   });
-});
+  $('html, body').animate({
+    scrollTop: $('.slider-element').offset().top
+  }, 500);
+}
