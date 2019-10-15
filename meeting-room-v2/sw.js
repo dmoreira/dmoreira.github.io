95d54084ed8ecffbd152e719afb3e6f74ef8b980
Davi Moreira <davi.moreira@vismaconsulting.fi>
2019-10-15 23:54:30 +0300

    adding service worker

---
+++ meeting-room-v2/sw.js
@@ -0,0 +1,37 @@
+const staticCacheName = 'static-meetingroom-cache-v1';
+const expectedCaches = [
+  staticCacheName
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => cache.addAll([
+      '/index.html',
+      '/index.js',
+      '/scripts.js',
+      '/styles.css'
+    ]))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(r => r || fetch(event.request))
+    .catch(e => {
+      console.log(e);
+    })
+  );
+});
