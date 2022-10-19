14edeeba795ae162db76bf08417cad1c6a20becd
dmoreira <davi_moreira@yahoo.com>
2022-10-19 12:00:52 +0300

    Update current-event-element.js

--- meeting-room-v2/src/web-components/current-event-element.js
+++ meeting-room-v2/src/web-components/current-event-element.js
@@ -46,7 +46,7 @@ class CurrentEventElement extends LitElement {
   render() {
     return html`
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      ${this.events.length >= 0 ? html`
+      ${this.events.length > 0 ? html`
 
         <div class="row">
           <div class="col-md-8">
